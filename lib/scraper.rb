e41fb476e5f021f153d30b3164f90af98babdb9d
Ebo Lee <yiboli.coding@gmail.com>
2020-08-17 04:46:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -46,8 +46,9 @@ class Scraper
 
     scraped_student[:profile_quote] = html.css("div.main-wrapper.profile").css("div.vitals-container").css("div.profile-quote").text
     scraped_student[:bio] = html.css("div.main-wrapper.profile").css("div.details-container").css("div.description-holder").css("p").text
-  binding.pry
+
     scraped_student
+      binding.pry
   end
 
 end
