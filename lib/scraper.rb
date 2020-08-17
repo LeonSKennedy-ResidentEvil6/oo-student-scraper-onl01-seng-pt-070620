eac2f2bffe044e8123273ab400dbf3a97d8686f0
Ebo Lee <yiboli.coding@gmail.com>
2020-08-17 04:21:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -32,7 +32,6 @@ class Scraper
     html = Nokogiri::HTML.parse(open(profile_url))
     # scrap each different social media link if there is one
     social_link = html.css("div.main-wrapper.profile").css("div.vitals-container").css("div.social-icon-container a").each do |social|
-
       if social.attribute("href").value.include?("twitter")
         scraped_student[:twitter] = social.attribute("href").value
       elsif social.attribute("href").value.include?("linkedin")
