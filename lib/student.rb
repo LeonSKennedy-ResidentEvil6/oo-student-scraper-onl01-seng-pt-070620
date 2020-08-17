fdf32c948b1969e93698bc0ed73463d99c9a53fe
Ebo Lee <yiboli.coding@gmail.com>
2020-08-17 05:01:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   def initialize(student_hash)
     student_hash.each {|key, value| self.send(("#{key}="), value)}
-    
+    @@all << Student.new(student_hash)
   end
 
   def self.create_from_collection(students_array)
