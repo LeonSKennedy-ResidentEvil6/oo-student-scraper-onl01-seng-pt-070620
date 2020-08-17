1e7c8481fc4e4fd77f2b5e124cf0832d035cd64f
Ebo Lee <yiboli.coding@gmail.com>
2020-08-17 05:12:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,8 @@ class Student
 
   def self.create_from_collection(students_array)
     new_student = Scraper.new(students_array)
-    
+    @@all << new_student
+
   end
 
   def add_student_attributes(attributes_hash)
