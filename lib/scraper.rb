9034fa7044f0f14189e085ead2f7008f2fff2f86
Ebo Lee <yiboli.coding@gmail.com>
2020-08-17 04:05:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,21 +5,21 @@ require 'pry'
 class Scraper
 
 
-  # def self.scrape_index_page(index_url)
-  #   scraped_students = []
-  #   html = open(index_url)
-  #   doc = Nokogiri::HTML(html)
-  #
-  #   doc.css("div.student-card").each do |student|
-  #     student_profile = {}
-  #     student_profile[:name] = student.css("h4.student-name").text
-  #     student_profile[:location] = student.css("p.student-location").text
-  #     student_profile_url = student.css("a").attribute("href").value
-  #     student_profile[:profile_url] = student_profile_url
-  #     scraped_students << student_profile
-  #   end
-  #   student
-  # end
+  def self.scrape_index_page(index_url)
+    scraped_students = []
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+
+    doc.css("div.student-card").each do |student|
+      student_profile = {}
+      student_profile[:name] = student.css("h4.student-name").text
+      student_profile[:location] = student.css("p.student-location").text
+      student_profile_url = student.css("a").attribute("href").value
+      student_profile[:profile_url] = student_profile_url
+      scraped_students << student_profile
+    end
+    student
+  end
 
 
 
