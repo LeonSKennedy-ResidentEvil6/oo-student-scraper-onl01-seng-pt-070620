5aae48909e5d989da4a4405cab098f9d203e9a3f
Ebo Lee <yiboli.coding@gmail.com>
2020-08-17 03:45:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     scraped_students = []
     html = open(index_url)
-    doc = Nokogiri::HTML()
+    doc = Nokogiri::HTML(html)
 
     doc.css("div.student-card").collect do |student|
       student_profile = {}
